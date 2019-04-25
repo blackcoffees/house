a67cd6c0149bf6368013e35a8c53f7b2632d5854
blackcoffees <389622979@qq.com>
2019-04-25 10:45:37 +0800

    '优化'

--- scrapy_spider/spiders/house.py
+++ scrapy_spider/spiders/house.py
@@ -247,63 +247,199 @@ class BuildingSpider(scrapy.Spider):
         pool.commit(update_sql, [update_status, datetime.datetime.now(), self.building.get("id")])
 
 
-class HouseSpider(scrapy.Spider):
+# class HouseSpider(scrapy.Spider):
+#
+#     name = "house"
+#     base_url = "http://www.cq315house.com/WebService/Service.asmx/GetRoomInfo"
+#     total_house = 0
+#     now_index = 0
+#     house = None
+#     is_change_proxy = get_switch_activity("proxy_house")
+#
+#     def start_requests(self):
+#         self.total_house = self.get_total_house()
+#         return [self.get_request()]
+#
+#     def parse(self, response):
+#         try:
+#             if not response.text:
+#                 raise BaseException(u"没有获取到数据")
+#             if not is_json(response.text):
+#                 raise BaseException(u"没有获取到数据")
+#             json_response = json.loads(response.text)
+#             room = json.loads(json_response.get("d"))
+#             now_status = BuildingSpider.get_house_status(BuildingSpider, room.get("status"))
+#             if now_status != int(self.house.get("status")):
+#                 new_description = json.dumps(dict(json.loads(self.house.get("description")), **room))
+#                 sql = """update house set status=%s, updated=%s, description=%s where id=%s"""
+#                 pool.commit(sql, [now_status, datetime.datetime.now(), new_description, self.house.get("id")])
+#                 logger.info(u"修改房屋状态:%s" % room.get("location"))
+#                 self.total_house = self.get_total_house()
+#             self.is_change_proxy = False
+#         except BaseException as e:
+#             logger.error(e)
+#             if e.message:
+#                 logger.error(e.message)
+#             self.is_change_proxy = True
+#         finally:
+#             yield self.get_request()
+#
+#     def get_total_house(self):
+#         sql = """select count(1) from house where status in (2,6)"""
+#         result = pool.find_one(sql)
+#         if result:
+#             return result.get("count(1)")
+#         return 0
+#
+#     def get_body(self):
+#         now_total_house = self.get_total_house()
+#         if now_total_house == self.total_house:
+#             self.now_index += 1
+#         sql = """select * from house where status in (2,6) limit %s,1"""
+#         self.house = pool.find_one(sql, [self.now_index])
+#         if self.house:
+#             return json.dumps({"roomId": self.house.get("web_house_id")})
+#         raise CloseSpider()
+#
+#     def get_request(self):
+#         header = {
+#             "Content-Type": "application/json",
+#         }
+#         request = Request(self.base_url, body=self.get_body(), headers=header, method="POST", dont_filter=True)
+#         return request
 
+
+class HouseSpider(scrapy.Spider):
     name = "house"
-    base_url = "http://www.cq315house.com/WebService/Service.asmx/GetRoomInfo"
-    total_house = 0
-    now_index = 0
-    house = None
     is_change_proxy = get_switch_activity("proxy_house")
+    base_sql = """select * from building where id in (select building_id from house where status in (2,6) GROUP BY building_id) limit %s,1;"""
+    all_houses_sql = """select * from house where building_id=%s"""
+    base_url = u"http://www.cq315house.com/WebService/Service.asmx/GetRoomJson"
+    now_db_index = 0
+    dict_all_houses = dict()
+    building = None
+    list_house_key = list()
 
     def start_requests(self):
-        self.total_house = self.get_total_house()
-        return [self.get_request()]
+        return [self.create_request()]
 
     def parse(self, response):
         try:
             if not response.text:
-                raise BaseException(u"没有获取到数据")
+                raise BaseException(u"需要切换代理")
             if not is_json(response.text):
-                raise BaseException(u"没有获取到数据")
-            json_response = json.loads(response.text)
-            room = json.loads(json_response.get("d"))
-            now_status = BuildingSpider.get_house_status(BuildingSpider, room.get("status"))
-            if now_status != int(self.house.get("status")):
-                new_description = json.dumps(dict(json.loads(self.house.get("description")), **room))
-                sql = """update house set status=%s, updated=%s, description=%s where id=%s"""
-                pool.commit(sql, [now_status, datetime.datetime.now(), new_description, self.house.get("id")])
-                logger.info(u"修改房屋状态:%s" % room.get("location"))
-                self.total_house = self.get_total_house()
+                raise BaseException(u"需要切换代理")
             self.is_change_proxy = False
-            yield self.get_request()
+            if "GetRoomJson" in response._url:
+                list_json_response = json.loads(json.loads(response.text).get("d").replace("\\", "\\\\"))
+                if not isinstance(list_json_response, list):
+                    raise BaseException(u"返回数据错误:%s" % list_json_response)
+                for item_json_data in list_json_response:
+                    for item_room in item_json_data.get("rooms"):
+                        if not item_room.get("location"):
+                            continue
+                        status = self.get_house_status(BuildingSpider, item_room.get("status"))
+                        key = item_room.get("id")
+                        if int(self.dict_all_houses.get(key).get("status")) != status:
+                            self.dict_all_houses.get(key)["status"] = status
+                            self.list_house_key.append(key)
+                            yield self.get_house_request()
+                if len(self.list_house_key) == 0:
+                    yield self.create_request()
+            elif "GetRoomInfo" in response._url:
+                json_response = json.loads(response.text)
+                room = json.loads(json_response.get("d"))
+                key = room.get("id")
+                new_description = json.dumps(dict(json.loads(self.dict_all_houses.get(key).get("description")), **room))
+                sql = """update house set status=%s, updated=%s, description=%s where id=%s"""
+                pool.commit(sql, [self.dict_all_houses.get(key).get("status"), datetime.datetime.now(), new_description,
+                                  self.dict_all_houses.get(key).get("id")])
+                logger.info(u"修改房屋状态:%s" % room.get("location"))
+                self.list_house_key.remove(key)
+                if len(self.list_house_key) == 0:
+                    yield self.create_request()
         except BaseException as e:
-            logger.error(e)
+            if type(e) == CloseSpider:
+                raise CloseSpider()
+            logger.warning(e)
+            if e.message:
+                logger.warning(e.message)
             self.is_change_proxy = True
-        finally:
-            yield self.get_request()
+            # if "GetRoomJson" in response._url:
+            #
+            # elif "GetRoomInfo" in response._url:
 
-    def get_total_house(self):
-        sql = """select count(1) from house where status in (2,6)"""
-        result = pool.find_one(sql)
-        if result:
-            return result.get("count(1)")
-        return 0
+    def get_request_body(self):
+        self.building = pool.find_one(self.base_sql, [self.now_db_index])
+        if not self.building:
+            raise CloseSpider()
+        self.now_db_index += 1
+        list_all_houses = pool.find(self.all_houses_sql, [self.building.get("id")])
+        self.dict_all_houses = dict()
+        for item_house in list_all_houses:
+            self.dict_all_houses[item_house.get("web_house_id")] = item_house
+        temp_dict = {"buildingid": self.building.get("web_building_id")}
+        return json.dumps(temp_dict)
 
-    def get_body(self):
-        now_total_house = self.get_total_house()
-        if now_total_house == self.total_house:
-            self.now_index += 1
-        sql = """select * from house where status in (2,6) limit %s,1"""
-        self.house = pool.find_one(sql, [self.now_index])
-        if self.house:
-            return json.dumps({"roomId": self.house.get("web_house_id")})
-        raise CloseSpider()
+    def create_request(self, url=None, callback=None, method="POST", body=None, headers=None):
+        if not url:
+            url = self.base_url
+        if not body:
+            body = self.get_request_body()
+        if not headers:
+            headers = {"Content-Type": "application/json", "Accept": "application/json, text/javascript, */*; q=0.01",
+                       "Host": "www.cq315house.com", "Origin": "http://www.cq315house.com",
+                       "Referer": "http://www.cq315house.com/HtmlPage/ShowRooms.html?buildingid=%s&block=%s" %
+                                  (self.building.get("web_building_id"), self.building.get("building_name"))}
+        if not callback:
+            callback = self.parse
+        return Request(url, callback=callback, method=method, body=body, headers=headers, dont_filter=True)
 
-    def get_request(self):
+    def get_house_request(self):
         header = {
             "Content-Type": "application/json",
         }
-        request = Request(self.base_url, body=self.get_body(), headers=header, method="POST", dont_filter=True)
+        request = Request("http://www.cq315house.com/WebService/Service.asmx/GetRoomInfo",
+                          body=json.dumps({"roomId": self.dict_all_houses.get(self.list_house_key[len(self.list_house_key)-1]).get("web_house_id")}),
+                          headers=header, method="POST", dont_filter=True)
         return request
 
+    @staticmethod
+    def get_house_status(self, status):
+        list_color = [
+            {"val": 8, "name": "已售", "ab": "已售", "bgColor": "#ff00ff", "ftColor": "#000000", "priority": 1, "type": 1,
+             "alarmType": 1, "showType": 0, "parentType": 0, "treeLevel": 0},
+            {"val": 64, "name": "不可售", "ab": "不可售", "bgColor": "#ffff00", "ftColor": "#000000", "priority": 2,
+             "type": 0,
+             "alarmType": 1, "showType": 0, "parentType": 0, "treeLevel": 0},
+            {"val": 256, "name": "不可售", "ab": "不可售", "bgColor": "#ffff00", "ftColor": "#000000", "priority": 3,
+             "type": 1,
+             "alarmType": 1, "showType": 0, "parentType": 0, "treeLevel": 0},
+            {"val": 1024, "name": "不可售", "ab": "不可售", "bgColor": "#ffff00", "ftColor": "#000000", "priority": 4,
+             "type": 1,
+             "alarmType": 1, "showType": 0, "parentType": 0, "treeLevel": 0},
+            {"val": 2048, "name": "已售", "ab": "已售", "bgColor": "#ff00ff", "ftColor": "#000000", "priority": 5,
+             "type": 1,
+             "alarmType": 1, "showType": 0, "parentType": 0, "treeLevel": 0},
+            {"val": 16777216, "name": "已售", "ab": "已售", "bgColor": "#ff00ff", "ftColor": "#000000", "priority": 5,
+             "type": 1, "alarmType": 1, "showType": 0, "parentType": 0, "treeLevel": 0},
+            {"val": 131072, "name": "不可售", "ab": "不可售", "bgColor": "#ffff00", "ftColor": "#000000", "priority": 6,
+             "type": 1, "alarmType": 1, "showType": 0, "parentType": 0, "treeLevel": 0},
+            {"val": 262146, "name": "未售", "ab": "可售", "bgColor": "#00ff00", "ftColor": "#000000", "priority": 7,
+             "type": 1,
+             "alarmType": 1, "showType": 0, "parentType": 0, "treeLevel": 0},
+            {"val": 262150, "name": "未售", "ab": "可售", "bgColor": "#00ff00", "ftColor": "#000000", "priority": 8,
+             "type": 1,
+             "alarmType": 1, "showType": 0, "parentType": 0, "treeLevel": 0},
+            {"val": 524292, "name": "未售", "ab": "可售", "bgColor": "#00ff00", "ftColor": "#000000", "priority": 10,
+             "type": 1, "alarmType": 1, "showType": 0, "parentType": 0, "treeLevel": 0},
+            {"val": 2097152, "name": "已售", "ab": "已售", "bgColor": "#ff00ff", "ftColor": "#000000", "priority": 11,
+             "type": 1, "alarmType": 1, "showType": 0, "parentType": 0, "treeLevel": 0},
+            {"val": 2621444, "name": "已售", "ab": "已售", "bgColor": "#ff00ff", "ftColor": "#000000", "priority": 12,
+             "type": 1, "alarmType": 1, "showType": 0, "parentType": 0, "treeLevel": 0},
+            {"val": 20176833, "name": "不可售", "ab": "不可售", "bgColor": "#ffff00", "ftColor": "#000000", "priority": 14,
+             "type": 1, "alarmType": 1, "showType": 0, "parentType": 0, "treeLevel": 0}]
+        for item_color in list_color:
+            if item_color.get("val") & status == item_color.get("val"):
+                return ColorStatus.get(item_color.get("bgColor"))
+        return 6
--- util/SwitchUtil.py
+++ util/SwitchUtil.py
@@ -3,6 +3,6 @@ from db.PoolDB import pool
 
 
 def get_switch_activity(switch_code):
-    sql = """select status from sys_switch where switch_code=%s"""
+    sql = """select status from sys_function_switch where switch_code=%s"""
     result = pool.find_one(sql, [switch_code])
     return result.get("status")
